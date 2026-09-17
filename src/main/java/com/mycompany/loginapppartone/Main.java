4ba4c96f020be2f691852f859729949405da8330
Student <Student@LabVM2049939.dxilfivdk24uvc3i2n405iqpzg.jnbx.internal.cloudapp.net>
2026-09-17 14:41:45 +0200

    Initial Commit

--- src/main/java/com/mycompany/loginapppartone/Main.java
+++ src/main/java/com/mycompany/loginapppartone/Main.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
  *
  * @author Student
  */
+/**
+ * Demo console app for Part 1 - Registration and login feature
+ */
 public class Main {
    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
